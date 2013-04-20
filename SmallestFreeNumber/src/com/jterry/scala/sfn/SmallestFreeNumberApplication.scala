b85310d7ab48fbc79d032f796a0daf0d5de8fc7b
Jerome Terry <jerome.l.terry@gmail.com>
2013-04-20 09:31:28 -0230

    Adding test methods
    Adding methods to test generating Lists from 0 to 100, excluding a
    single value. The value excluded from the list will be the smallest free
    number, so we just check that getMinFree returns the omitted value

--- SmallestFreeNumber/src/com/jterry/scala/sfn/SmallestFreeNumberApplication.scala
+++ SmallestFreeNumber/src/com/jterry/scala/sfn/SmallestFreeNumberApplication.scala
@@ -1,27 +1,44 @@
 package com.jterry.scala.sfn
 
 object SmallestFreeNumberApplication extends App {
-  println(getMinFree(Array(0, 1, 2, 4, 5, 6)).toString())
-	
-  def getMinFree (xs : Array[Int]) : Int = {
+  generateAndTestLists(101)
+  
+  def generateAndTestLists (n : Int) = {
+    for (i <- List.range(0, n)) {
+      testGetMinFree(generateList(i, n), i)
+    }
+  }
+  
+  def testGetMinFree(xs : List[Int], e : Int) = {
+    getMinFree(xs) - e == 0 match {
+      case true => println(e.toString() + " Ok")
+      case _    => println(e.toString() + " Failed")
+    }
+  } 
+  
+  def generateList (e : Int, n : Int) : List[Int] = {
+    return List.range(0,e) ::: List.range(e+1,n)
+  }
+  
+  def getMinFree (xs : List[Int]) : Int = {
     return getMinFreePrime(xs, 0, xs.length)
   }
 	
-  def getMinFreePrime (xs : Array[Int], start : Int, len : Int) : Int = {
+  def getMinFreePrime (xs : List[Int], start : Int, len : Int) : Int = {
     return len match {
       case 0 => start
       case _ => getMinFreeByPartition(xs, computePartition(start, len), start, len) 
     } 
   }
   
-  def getMinFreeByPartition (xs : Array[Int], pv : Int, start : Int, len : Int) : Int = { 
+  def getMinFreeByPartition (xs : List[Int], pv : Int, start : Int, len : Int) : Int = { 
     return partitionList(xs, pv) match {
       case (left, right) => getMinFreeByParts(left, right, start, len, pv)
 	  case _             => -1
 	}
   }
   
-  def getMinFreeByParts(left: Array[Int], right: Array[Int], start : Int, len: Int, pv : Int) : Int = {
+  def getMinFreeByParts(left: List[Int], right: List[Int], start : Int, len: Int, pv : Int) : Int = {
     return start + left.length - pv == 0 match {
       case true => getMinFreePrime(right, pv, right.length)
       case _    => getMinFreePrime(left, start, left.length)
@@ -32,7 +49,7 @@ object SmallestFreeNumberApplication extends App {
     return start + 1 + len / 2
   }
 	
-  def partitionList (xs : Array[Int], pv : Int) : (Array[Int], Array[Int]) = {
+  def partitionList (xs : List[Int], pv : Int) : (List[Int], List[Int]) = {
     return xs.partition(_ < pv);
   }
 }
